8ca42e8bfe41946013732c8771c53cf3c378e03e
Taha Eid <10812022101005@stud.cu.edu.eg>
2024-12-22 01:08:04 +0200

    Add files via upload

---
+++ client/src/App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import Navbar from "./components/navbar/Navbar";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Footer from "./components/Footer/Footer";
+
+
+
+const App = () => {
+
+  
+
+  return (
+    <>
+      
+      <div className="app">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </div>
+      <Footer />
+    </>
+  );
+};
+
+export default App;
