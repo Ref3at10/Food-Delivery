3f0a824febf31f2f8dcc2861d151833ce39f38f1
magdy <magdyelkhateeb444@gmail.com>
2024-12-22 02:57:31 +0200

    Fetch food data on frontend from databse & Create Shop Cart functionality

--- backend/server.js
+++ backend/server.js
@@ -2,9 +2,9 @@ import express from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/FoodRoute.js"
-import userRouter from "./routes/UserRoute.js"
-import "dotenv/config";
-
+import userRouter from "./routes/userRouter.js"
+import 'dotenv/config'
+import cartRouter from "./routes/cartRoute.js"
 
 // app config
 
@@ -21,7 +21,8 @@ app.use(cors())
 // api endpoints
 app.use("/api/food", foodRouter);
 app.use("/images" , express.static('uploads'))
-app.use("/api/user" , userRouter);
+app.use("/api/user",userRouter)
+app.use("/api/cart",cartRouter)
 
 app.get("/" , (req , res) => {
     res.send("API working")
