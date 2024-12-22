81b5572379b83efac49c3fb23ec5e7d9f812e067
magdy <magdyelkhateeb444@gmail.com>
2024-12-22 03:05:31 +0200

    add page cart

--- client/src/pages/Cart/Cart.js
+++ client/src/pages/Cart/Cart.js
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom'
 
 const Cart = () => {
 
-  const {cartItems , food_list , removeFromCart, getTotalCartAmount , url} = useContext(StoreContext)
+  const {cartItems , food_list , removeFromCart, getTotalCartAmount,url} = useContext(StoreContext)
 
   const navigate = useNavigate();
 
@@ -27,14 +27,13 @@ const Cart = () => {
             return (
               <div>
                 <div className="cart-items-title cart-items-item">
-                  <img src={item.image} alt="" />
-                  <img src={url + "/images/" + item.image} alt="" />
+                  <img src={url+"/images/"+item.image} alt="" />
                   <p>{item.name}</p>
                   <p>${item.price}</p>
                   <p>{cartItems[item._id]}</p>
                   <p>${item.price * cartItems[item._id]}</p>
                   <p onClick={() => removeFromCart(item._id)} className="cross">
-                    X
+                    x
                   </p>
                 </div>
                 <hr />
